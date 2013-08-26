5fbcfa3c4058b79fe2457a3e2940db775934c46d
RobQuistNL <rob.quist@dukesoft.nl>
2013-08-26 13:18:50 +0200

    Removed old code

--- GMHydraDll/GMHydraDll/GMHydraDll.cpp
+++ GMHydraDll/GMHydraDll/GMHydraDll.cpp
@@ -4,53 +4,6 @@
 
 sixenseAllControllerData acd;
 
-int max(int arg0, int arg1) {
-	if (arg0 == arg1) {
-		return arg0;
-	}
-	if (arg0 > arg1) {
-		return arg0;
-	} else {
-		return arg1;
-	}
-}
-
-class Quaternion {
-	public:
-	float w, x, y, z;
-};
-
-void CalculateRotation( Quaternion& q, double joyIndex ) {
-  float trace = acd.controllers[(int)joyIndex].rot_mat[0][0] + acd.controllers[(int)joyIndex].rot_mat[1][1] + acd.controllers[(int)joyIndex].rot_mat[2][2]; // I removed + 1.0f; see discussion with Ethan
-  if( trace > 0 ) {// I changed M_EPSILON to 0
-    float s = 0.5f / sqrtf(trace+ 1.0f);
-    q.w = 0.25f / s;
-    q.x = ( acd.controllers[(int)joyIndex].rot_mat[2][1] - acd.controllers[(int)joyIndex].rot_mat[1][2] ) * s;
-    q.y = ( acd.controllers[(int)joyIndex].rot_mat[0][2] - acd.controllers[(int)joyIndex].rot_mat[2][0] ) * s;
-    q.z = ( acd.controllers[(int)joyIndex].rot_mat[1][0] - acd.controllers[(int)joyIndex].rot_mat[0][1] ) * s;
-  } else {
-    if ( acd.controllers[(int)joyIndex].rot_mat[0][0] > acd.controllers[(int)joyIndex].rot_mat[1][1] && acd.controllers[(int)joyIndex].rot_mat[0][0] > acd.controllers[(int)joyIndex].rot_mat[2][2] ) {
-      float s = 2.0f * sqrtf( 1.0f + acd.controllers[(int)joyIndex].rot_mat[0][0] - acd.controllers[(int)joyIndex].rot_mat[1][1] - acd.controllers[(int)joyIndex].rot_mat[2][2]);
-      q.w = (acd.controllers[(int)joyIndex].rot_mat[2][1] - acd.controllers[(int)joyIndex].rot_mat[1][2] ) / s;
-      q.x = 0.25f * s;
-      q.y = (acd.controllers[(int)joyIndex].rot_mat[0][1] + acd.controllers[(int)joyIndex].rot_mat[1][0] ) / s;
-      q.z = (acd.controllers[(int)joyIndex].rot_mat[0][2] + acd.controllers[(int)joyIndex].rot_mat[2][0] ) / s;
-    } else if (acd.controllers[(int)joyIndex].rot_mat[1][1] > acd.controllers[(int)joyIndex].rot_mat[2][2]) {
-      float s = 2.0f * sqrtf( 1.0f + acd.controllers[(int)joyIndex].rot_mat[1][1] - acd.controllers[(int)joyIndex].rot_mat[0][0] - acd.controllers[(int)joyIndex].rot_mat[2][2]);
-      q.w = (acd.controllers[(int)joyIndex].rot_mat[0][2] - acd.controllers[(int)joyIndex].rot_mat[2][0] ) / s;
-      q.x = (acd.controllers[(int)joyIndex].rot_mat[0][1] + acd.controllers[(int)joyIndex].rot_mat[1][0] ) / s;
-      q.y = 0.25f * s;
-      q.z = (acd.controllers[(int)joyIndex].rot_mat[1][2] + acd.controllers[(int)joyIndex].rot_mat[2][1] ) / s;
-    } else {
-      float s = 2.0f * sqrtf( 1.0f + acd.controllers[(int)joyIndex].rot_mat[2][2] - acd.controllers[(int)joyIndex].rot_mat[0][0] - acd.controllers[(int)joyIndex].rot_mat[1][1] );
-      q.w = (acd.controllers[(int)joyIndex].rot_mat[1][0] - acd.controllers[(int)joyIndex].rot_mat[0][1] ) / s;
-      q.x = (acd.controllers[(int)joyIndex].rot_mat[0][2] + acd.controllers[(int)joyIndex].rot_mat[2][0] ) / s;
-      q.y = (acd.controllers[(int)joyIndex].rot_mat[1][2] + acd.controllers[(int)joyIndex].rot_mat[2][1] ) / s;
-      q.z = 0.25f * s;
-    }
-  }
-}
-
 GMEXPORT double GMH_updateStatus() {
 	sixenseGetAllNewestData( &acd );
 	return (double)1;
@@ -66,65 +19,22 @@ GMEXPORT double GMH_getJoystickY(double joyIndex) {
 GMEXPORT double GMH_getJoystickZ(double joyIndex) {
 	return (double) acd.controllers[(int)joyIndex].pos[2];
 }
-/*
+
+// Pitch roll and Yaw
 GMEXPORT double GMH_getJoystickRoll(double joyIndex) {
 	double roll;
-	roll = atan2(acd.controllers[(int)joyIndex].rot_mat[2][0], acd.controllers[(int)joyIndex].rot_mat[2][1]);
+
 	return (double) roll;
 }
 
 GMEXPORT double GMH_getJoystickPitch(double joyIndex) {
 	double pitch;
-	pitch = acos(acd.controllers[(int)joyIndex].rot_mat[2][2]);
+
 	return (double) pitch;
 }
 
 GMEXPORT double GMH_getJoystickYaw(double joyIndex) {
 	double yaw;
-	yaw = -atan2(acd.controllers[(int)joyIndex].rot_mat[0][2], acd.controllers[(int)joyIndex].rot_mat[1][2]);
-	return (double) yaw;
-}
-*/
-
-
-/*
-public static Quaternion QuaternionFromMatrix(Matrix4x4 m)
-{
-// Adapted from: http://www.euclideanspace.com/maths/geometry/rotations/conversions/matrixToQuaternion/index.htm
-w = sqrt(max(0, 1 + acd.controllers[(int)joyIndex].rot_mat[0][0] + acd.controllers[(int)joyIndex].rot_mat[1][1] + acd.controllers[(int)joyIndex].rot_mat[2][2])) / 2;
-x = sqrt(max(0, 1 + acd.controllers[(int)joyIndex].rot_mat[0][0] - acd.controllers[(int)joyIndex].rot_mat[1][1] - acd.controllers[(int)joyIndex].rot_mat[2][2])) / 2;
-y = sqrt(max(0, 1 - acd.controllers[(int)joyIndex].rot_mat[0][0] + acd.controllers[(int)joyIndex].rot_mat[1][1] - acd.controllers[(int)joyIndex].rot_mat[2][2])) / 2;
-z = sqrt(max(0, 1 - acd.controllers[(int)joyIndex].rot_mat[0][0] - acd.controllers[(int)joyIndex].rot_mat[1][1] + acd.controllers[(int)joyIndex].rot_mat[2][2])) / 2;
-x = x * Mathf.Sign(x * (acd.controllers[(int)joyIndex].rot_mat[2][1] - acd.controllers[(int)joyIndex].rot_mat[1][2]));
-y = y * Mathf.Sign(y * (acd.controllers[(int)joyIndex].rot_mat[0][2] - acd.controllers[(int)joyIndex].rot_mat[2][0]));
-z = z * Mathf.Sign(z * (acd.controllers[(int)joyIndex].rot_mat[1][0] - acd.controllers[(int)joyIndex].rot_mat[0][1]));
-}*/
-
-GMEXPORT double GMH_getJoystickRoll(double joyIndex) {
-	double roll;
-	//roll = atan2(acd.controllers[(int)joyIndex].rot_mat[2][0], acd.controllers[(int)joyIndex].rot_mat[2][1]);
-	Quaternion q;
-	CalculateRotation( q, joyIndex );
-	roll =  atan2(2*(q.x*q.y + q.w*q.z), q.w*q.w + q.x*q.x - q.y*q.y - q.z*q.z);
-	return (double) roll;
-}
-
-GMEXPORT double GMH_getJoystickPitch(double joyIndex) {
-	double pitch;
-	//pitch = acos(acd.controllers[(int)joyIndex].rot_mat[2][2]);
-	Quaternion q;
-	CalculateRotation( q, joyIndex );
-	pitch = atan2(2*(q.y*q.z + q.w*q.x), q.w*q.w - q.x*q.x - q.y*q.y + q.z*q.z);
-	return (double) pitch;
-}
-
-GMEXPORT double GMH_getJoystickYaw(double joyIndex) {
-	double yaw;
-	//yaw = -atan2(acd.controllers[(int)joyIndex].rot_mat[0][2], acd.controllers[(int)joyIndex].rot_mat[1][2]);
-	Quaternion q;
-	CalculateRotation( q, joyIndex );
-
-	yaw = asin(-2*(q.x*q.z - q.w*q.y));
 
 	return (double) yaw;
 }
